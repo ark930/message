dfd825b47c08f5ebefe16fbef37ee246f2cd032c
edwin <lark930@gmail.com>
2016-06-02 00:57:08 +0800

    调整路由结构

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,23 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['prefix' => 'api/v1'], function() {
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@login');
 
-Route::post('api/register', 'UserController@register');
-Route::post('api/login', 'UserController@login');
-
-Route::group(
-    [
-        'prefix' => 'api/v1',
-        'middleware' => 'auth:api',
-//        'middleware'=>'throttle:5',
-    ],
-    function () {
-        Route::get('usr/follow', 'UserController@getFollow');
-        Route::post('usr/follow/{f_user_id}', 'UserController@follow');
-        Route::post('usr/unfollow/{f_user_id}', 'UserController@unfollow');
+    Route::group([
+        'middleware' => [
+            'auth:api',
+//            'throttle:5',
+        ]
+    ], function () {
+        Route::get('user/follow', 'UserController@getFollow');
+        Route::post('user/follow/{f_user_id}', 'UserController@follow');
+        Route::post('user/unfollow/{f_user_id}', 'UserController@unfollow');
 
         Route::get('user/group', 'GroupController@show');
         Route::post('user/group/create', 'GroupController@createGroup');
@@ -35,13 +31,8 @@ Route::group(
         Route::post('user/group/{group_id}/quit', 'GroupController@quit');
         Route::post('user/group/{group_id}/dismiss', 'GroupController@dismiss');
         Route::post('user/group/{group_id}/invite', 'GroupController@invite');
-        
+
         Route::post('message', 'MessageController@sendMessage');
         Route::get('message', 'MessageController@getMessage');
     });
-
-Route::auth();
-
-Route::get('/home', 'HomeController@index');
-
-Route::resource('test','TestController');
+});
